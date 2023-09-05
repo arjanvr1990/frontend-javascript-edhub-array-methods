9c00e6c36a76cff89e2cefa1289c47744ebfb2e8
arjanvr1990 <arjanvanraamsdonk@gmail.com>
2023-09-05 14:14:42 +0200

    opdracht gemaakt

--- arrayMethods.js
+++ arrayMethods.js
@@ -19,16 +19,56 @@ const inventors = [
 // Verwachte uitkomst:
 // [{ first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 }, { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 }]
 
+const bornIn1500 = inventors.filter((yearBorn1500) => {
+  return yearBorn1500.year >= 1500 && yearBorn1500.year <= 1600;
+});
 
+console.log(bornIn1500);
 
 // 2. Maak een array met daarin alle geboortejaren van de uitvinders
 // Verwachte uitkomst: [1879, 1643, 1564, 1867, 1571, 1473, 1858, 1898, 1815, 1855, 1878, 1847];
+
+function bornIn(inventors) {
+let yearOfBorn = [];
+  for (let i = 0; i < inventors.length; i++) {
+  yearOfBorn.push (inventors[i].year);
+  }
+  return yearOfBorn;
+}
+
+console.log(bornIn(inventors));
+
+// const bornIn = inventors.filter((yearborn) => {
+//   let yearOfBorn = 0;
+//   return yearborn.year ;
+// });
+//
+// console.log(inventors[0].year);
+
+// const yearOfBorn = inventors.filter((inventor) => {
+//   console.log(inventors.year);
+//   if (inventors.year === inventors.year);
+//   {
+//     return true
+//   }
+//     return false
+// });
+
+
+// Oplossing van ChatGPT
+const geboortejaren = inventors.map(inventor => inventor.year);
+
+console.log(geboortejaren);
+
 
 
 
 // 3. Maak een array met daarin alle volledige namen van de uitvinders (dus voor- en achternaam als één string)
 // Verwachte uitkomst: [ 'Albert Einstein', 'Isaac Newton', 'Galileo Galilei', 'Marie Curie', 'Johannes Kepler', 'Nicolaus Copernicus', 'Max Planck', 'Katherine Blodgett', 'Ada Lovelace', 'Sarah E. Goode', 'Lise Meitner', 'Thomas Edison']
 
+const firstAndLastName = inventors.map(inventor => inventor.first + " " + inventor.last);
+
+console.log(firstAndLastName);
 
 
 // 4. Sorteer de uitvinders op geboortejaar, oplopend van oudste naar jongste uitvinder
@@ -48,7 +88,17 @@ const inventors = [
 //   { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
 // ]
 
+inventors.sort((a, b) => {
+  if (a.year > b.year) {
+    return 1;
+  }
+  if (a.year < b.year) {
+    return -1;
+  }
+  return 0;
+})
 
+console.log(inventors);
 
 // 5. Sorteer de uitvinders op hoeveel jaren ze geleefd hebben, van langste leven naar kortste leven
 // Verwachte uitkomst:
@@ -67,8 +117,40 @@ const inventors = [
 // { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 }
 // ]
 
+const yearsLived = inventors.map(inventor => inventor.passed - inventor.year);
 
+inventors.sort((a, b) => {
+  if (a.passed - a.year > b.passed - b.year) {
+    return 1;
+  }
+  if (a.passed - a.year < b.passed - b.year) {
+    return -1;
+  }
+  return 0;
+})
 
+console.log(inventors);
+
+inventors.sort((a, b) => {
+  if (a.year - a.passed > b.year - b.passed) {
+    return 1;
+  }
+  if (a.year - a.passed < b.year - b.passed) {
+    return -1;
+  }
+  return 0;
+})
+
+console.log(inventors);
 
 // 6. Vind de gegevens over de uitvinder wiens achternaam 'Edison' is.
 // Verwachte uitkomst: { first: 'Thomas', last: 'Edison', year: 1847, passed: 1931 }
+
+const findEdison = inventors.find((inventor)  => {
+  if (inventor.last === "Edison") {
+    return true;
+  }
+  return false;
+});
+
+console.log(findEdison);
